5e0f938def6cf90497c5d55f861cb08d622bfd64
MQSCarlosEstrada <cestrada@maquila-software.com>
2023-08-03 14:03:42 -0700

    ajax forms
    ajax forms

---
+++ InvoiceManagement/Scripts/site.js
@@ -0,0 +1,20 @@
+﻿function GetView(url, content) {
+    $.ajax({
+        type: "GET",
+        cache: false,
+        url: url,
+    }).done(function (data) {
+        $('#' + content).html(data).ready();
+    }).fail(function () {
+
+    });
+}
+
+function GetResponse(response) {
+    if (response.error != true) {
+        swal("", "Registro guardado con exito!", "success");
+    }
+    else {
+        swal("", response.message, "error");
+    }
+}
---
+++ InvoiceManagement/Utils/ExceptionHandler.cs
@@ -0,0 +1,46 @@
+﻿using InvoiceManagement.Models;
+using System;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Text;
+
+namespace InvoiceManagement.Utils
+{
+    public class ExceptionHandler
+    {
+        public static void ResolveException(DbEntityValidationException ex, ref Response response)
+        {
+            response.Error = true;
+            foreach (var eve in ex.EntityValidationErrors)
+            {
+                foreach (var ve in eve.ValidationErrors)
+                    response.Messages.Add(ve.ErrorMessage.Replace("'", "").Replace("\"", "").Replace("\r\n", "  "));
+            }
+        }
+
+        public static void ResolveException(DbUpdateException ex, ref Response response)
+        {
+            response.Error = true;
+            var builder = new StringBuilder("A DbUpdateException was caught while saving changes. ");
+            try
+            {
+                foreach (var result in ex.Entries)
+                    builder.AppendFormat("Type: {0} was part of the problem. ", result.Entity.GetType().Name);
+            }
+            catch (Exception e)
+            {
+                builder.Append("Error parsing DbUpdateException: " + e.ToString());
+            }
+            response.Messages.Add(builder.ToString());
+        }
+
+        public static void ResolveException(Exception ex, ref Response response)
+        {
+            response.Error = true;
+            if (ex.InnerException != null)
+                response.Messages.Add(ex.InnerException.InnerException.Message);
+            else
+                response.Messages.Add(ex.Message);
+        }
+    }
+}
